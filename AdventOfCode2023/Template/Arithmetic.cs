16eae22377647fe7a03adb310d19a5ec11d8de56
Edouard Chevalier <echevalier@configit.com>
2023-12-11 09:53:54 +0100

    day11

---
+++ AdventOfCode2023/Template/Arithmetic.cs
@@ -0,0 +1,29 @@
+﻿namespace Template;
+
+public class Arithmetic {
+    
+    /// <summary>
+    /// Plus grand denominateur commun
+    /// </summary>
+    public static long gcd(long n1, long n2) {
+        if (n2 == 0) {
+            return n1;
+        }
+
+        return gcd(n2, n1 % n2);
+    }
+    
+    /// <summary>
+    /// Plus grand denominateur commun
+    /// </summary>
+    public static long gcd(params long [] numbers) {
+        return numbers.Aggregate(gcd);
+    }
+
+    /// <summary>
+    /// Plus petit multiple commun
+    /// </summary>
+    public static long lcm(params long[] numbers) {
+        return numbers.Aggregate((s, val) => s * val / gcd(s, val));
+    }
+}
---
+++ AdventOfCode2023/Template/DisjointSet.cs
@@ -0,0 +1,33 @@
+﻿namespace Template;
+
+public class DisjointSet<T> where T : IEquatable<T> {
+    
+    private readonly Dictionary<T, T> _parents = new ();
+    
+    public void Add( T cell) {
+        _parents[cell] = cell;
+    }
+    
+    public T Find( T cell) {
+        if (_parents[cell].Equals( cell)) {
+            return cell;
+        }
+
+        return _parents[cell] = Find(_parents[cell]);
+    }
+    
+    public void Union( T cell1, T cell2) {
+        var p1 = Find(cell1);
+        var p2 = Find(cell2);
+        if (p1.Equals(p2)) {
+            return;
+        }
+
+        _parents[p1] = p2;
+    }
+
+    /// <summary>
+    /// Returns a lookup of the parents to the children
+    /// </summary>
+    public ILookup<T, T> ToLookup() => _parents.ToLookup(kv => kv.Value, kv => kv.Key);
+}
