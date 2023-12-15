ecfb59a0855d00de8f2d027d13ea4197a6b4a6b1
Edouard Chevalier <echevalier@configit.com>
2023-12-15 07:10:44 +0100

    day15

--- AdventOfCode2023/Day16/Program.cs
+++ AdventOfCode2023/Day16/Program.cs
@@ -6,9 +6,6 @@ Console.WriteLine("Hello, World!");
 const string inputReal = @"";
 const string inputTest = @"";
 
-var input = inputTest;
-var lines = input.Split(Environment.NewLine);
-
 void Part1(string input) {
     var lines = input.Split(Environment.NewLine);
 }
