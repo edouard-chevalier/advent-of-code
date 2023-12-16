cea2b3c0114061d24b2e010068d6f12b84ba5ba6
Edouard Chevalier <echevalier@configit.com>
2023-12-16 08:46:30 +0100

    day16

---
+++ AdventOfCode2023/Day17/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, World!");
+
+const string inputReal = @"";
+const string inputTest = @"";
+
+var input = inputTest;
+var lines = input.Split(Environment.NewLine);
+
+void Part1(string input) {
+    var lines = input.Split(Environment.NewLine);
+}
+
+Part1(inputTest);
---
+++ AdventOfCode2023/Template/Grid.cs
@@ -0,0 +1,67 @@
+﻿namespace Template;
+
+/// <summary>
+/// Helper for grids
+/// </summary>
+public static class Grid
+{
+    public static bool HasCell(this char[][] grid, int i, int j) {
+        return i>=0 && i<grid.Length && j>=0 && j<grid[0].Length;
+    }
+    
+    public static int CountCell(this char[][] grid, char c) {
+        int count = 0;
+        foreach(var row in grid) {
+            foreach(var cell in row) {
+                if( cell == c) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static int CountCell(this char[][] grid, Predicate<char> predicate){
+        int count = 0;
+        foreach(var row in grid) {
+            foreach(var cell in row) {
+                if( predicate(cell)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    public static bool GoRight(this char[][] grid, int i, ref int j) {
+        if( grid.HasCell(i, j + 1)) {
+            j++;
+            return true;
+        }
+
+        return false;
+    }
+    public static bool GoLeft(this char[][] grid, int i, ref int j) {
+        if( grid.HasCell(i, j - 1)) {
+            j--;
+            return true;
+        }
+
+        return false;
+    }
+    public static bool GoUp(this char[][] grid, ref int i, int j) {
+        if( grid.HasCell(i - 1, j)) {
+            i--;
+            return true;
+        }
+
+        return false;
+    }
+    public static bool GoDown(this char[][] grid, ref int i, int j) {
+        if( grid.HasCell(i + 1, j)) {
+            i++;
+            return true;
+        }
+
+        return false;
+    }
+}
