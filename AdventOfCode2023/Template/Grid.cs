1a19a4025bc5b73747c80b9a11a126ef7152c691
Edouard Chevalier <echevalier@configit.com>
2023-12-17 09:16:19 +0100

    day 17

--- AdventOfCode2023/Template/Grid.cs
+++ AdventOfCode2023/Template/Grid.cs
@@ -5,7 +5,7 @@
 /// </summary>
 public static class Grid
 {
-    public static bool HasCell(this char[][] grid, int i, int j) {
+    public static bool HasCell<T>(this T[][] grid, int i, int j) {
         return i>=0 && i<grid.Length && j>=0 && j<grid[0].Length;
     }
     
@@ -20,7 +20,7 @@ public static class Grid
         }
         return count;
     }
-    public static int CountCell(this char[][] grid, Predicate<char> predicate){
+    public static int CountCell<T>(this T[][] grid, Predicate<T> predicate){
         int count = 0;
         foreach(var row in grid) {
             foreach(var cell in row) {
@@ -32,7 +32,7 @@ public static class Grid
         return count;
     }
     
-    public static bool GoRight(this char[][] grid, int i, ref int j) {
+    public static bool GoRight<T>(this T[][] grid, int i, ref int j) {
         if( grid.HasCell(i, j + 1)) {
             j++;
             return true;
@@ -40,7 +40,7 @@ public static class Grid
 
         return false;
     }
-    public static bool GoLeft(this char[][] grid, int i, ref int j) {
+    public static bool GoLeft<T>(this T[][] grid, int i, ref int j) {
         if( grid.HasCell(i, j - 1)) {
             j--;
             return true;
@@ -48,7 +48,7 @@ public static class Grid
 
         return false;
     }
-    public static bool GoUp(this char[][] grid, ref int i, int j) {
+    public static bool GoUp<T>(this T[][] grid, ref int i, int j) {
         if( grid.HasCell(i - 1, j)) {
             i--;
             return true;
@@ -56,7 +56,7 @@ public static class Grid
 
         return false;
     }
-    public static bool GoDown(this char[][] grid, ref int i, int j) {
+    public static bool GoDown<T>(this T[][] grid, ref int i, int j) {
         if( grid.HasCell(i + 1, j)) {
             i++;
             return true;
